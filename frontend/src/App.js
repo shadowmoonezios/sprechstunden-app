969ec96b19145f844e5547f5b1e010727b305dd1
shadowmoonezios <agushifelde@hotmail.com>
2025-11-02 12:16:27 +0200

    feat: initial commit for frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Willkommen zur Sprechstunden-App</h1>
+    </div>
+  );
+};
+
+export default App;
